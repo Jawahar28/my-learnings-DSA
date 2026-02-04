779e8ea00587a9bffe9c3b91c86d16f8b71e9edd
Jawahar28 <jawaharn246@gmail.com>
2026-02-04 13:32:43 +0530

    Problems for Arrays

--- Day-1-Arrays-problems.py
+++ Day-1-Arrays-problems.py
@@ -31,3 +31,73 @@ k = 4
 k = k%len(arr)
 print(arr[k:]+arr[:k]) # Rotate Left
 print(arr[-k:]+arr[:-k]) # Rotate Right'''
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Revision :
+
+
+# Q1: Reverse the array in 3 methods.
+
+# Using Two pointers:
+'''def reverse_arr(arr):
+    i,j = 0, len(arr)-1
+    while i < j:
+        arr[i],arr[j] = arr[j],arr[i]
+        i+=1
+        j-=1
+    return arr'''
+
+# Using slicing:
+'''def reverse_arr(arr):
+    return arr[::-1]'''
+
+# Using loop:
+'''def reverse_arr(arr):
+    ans = []
+    for i in range(len(arr)-1,-1,-1):
+        ans.append(arr[i])
+    return ans
+
+
+print(reverse_arr([3,4,5,6,2]))'''
+
+
+# Q2: Rotate array k times(left):
+# Using Slicing
+'''def rotate_arr(arr,k):
+    k%=len(arr)
+    return f"left rotated arr : {arr[k:]+arr[:k]}"
+
+def rotate_arr(arr,k):
+    k%=len(arr)
+    return f"right rotated arr : {arr[-k:]+arr[:-k]}"'''
+
+
+# Using functions:
+'''def rotate_arr(arr,k):
+    k%=len(arr)
+
+    print("Rotate Right :")
+    reverse_arr(arr,0,len(arr)-1)
+    reverse_arr(arr,0,k-1)
+    reverse_arr(arr,k,len(arr)-1)
+    return arr
+
+    print("Rotate Left :")
+    reverse_arr(arr,0,k-1)
+    reverse_arr(arr,k,len(arr)-1)
+    reverse_arr(arr,0, len(arr)-1)
+    return arr
+
+def reverse_arr(arr,i,j):
+    while i<j:
+        arr[i],arr[j] = arr[j],arr[i]
+        i+=1
+        j-=1
+    return arr
+
+
+print(rotate_arr([2,4,2,1,5],2))'''
+
+
